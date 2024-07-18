c2b47a42d84d8bb960ef7a3eaaf9a04606ffc9d2
Ton Arruda <georgem.juniore@gmail.com>
2024-07-18 10:55:51 -0300

    Inclusão de arquivo .env

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,18 +1,20 @@
-describe('Login Tests', () => {
-  beforeEach('', () => {
-    cy.visit('/login')
+describe("Login Tests", () => {
+
+  beforeEach("", () => {
+    cy.visit("/login");
+    cy.url().should("eq", `${Cypress.config("baseUrl")}/login`);
+  });
+
+  it("Tentativa de Login com sucesso", () => {
+    cy.login(Cypress.env('email'), Cypress.env('password'))
+    cy.get(".swal2-popup").should("be.visible");
+    cy.contains("Login realizado").should("be.visible");
+    cy.contains("OK").should("be.visible").click();
+    cy.url().should("eq", `${Cypress.config("baseUrl")}/my-account`);
+  });
+
+  it("Tentativa de Login sem sucesso", () => {
+    cy.login("email", "P@ssw0rd!");
+    cy.contains("E-mail inválido").should("be.visible");
+  });
 });
-
-it('Tentativa de Login com sucesso', () => {
-  cy.login('email@email.com', 'P@ssw0rd!')
-  cy.get('.swal2-popup').should('be.visible')
-  cy.contains('Login realizado').should('be.visible')
-  cy.contains('OK').should('be.visible').click()
-  cy.url().should('eq', 'https://automationpratice.com.br/my-account');
-})
-
-  it('Tentativa de Login sem sucesso', () => {
-    cy.login('email', 'P@ssw0rd!')
-    cy.contains('E-mail inválido').should('be.visible')
-  })
-})
