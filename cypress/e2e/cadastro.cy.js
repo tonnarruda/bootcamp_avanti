f3fd9098676200bff078bef4db69f202dd4b2801
Marillia Dantas <marillia.dantas@hotmail.com>
2024-08-12 08:43:45 -0300

    add mais cenarios de cadastro

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -4,6 +4,8 @@ const name = faker.person.fullName()
 const email = faker.internet.email()
 const password = faker.internet.password(6);
 const passwordMsg = "O campo senha deve ter pelo menos 6 dígitos";
+const requiredEmail = "O campo e-mail deve ser prenchido corretamente";
+const requiredNome= "O campo nome deve ser prenchido"
 
 describe('Cadastro', () => {
   beforeEach(() => {
@@ -11,22 +13,57 @@ describe('Cadastro', () => {
     cy.url().should("eq", `${Cypress.config("baseUrl")}/register`);
   });
 
-  it('CT009 - Tentativa de Cadastro com sucesso', () => {
+  it('CT001 - Tentativa de Cadastro com sucesso', () => {
     cy.cadastro(name, email, password);
     cy.get("#swal2-title").should("be.visible");
     cy.contains("Cadastro realizado!").should("be.visible");
     cy.contains(`Bem-vindo ${name}`).should("be.visible");
-    cy.contains("OK").should("be.visible").click();
     cy.url().should("eq", `${Cypress.config("baseUrl")}/my-account`);
   });
 
-  it('CT016 - Tentativa de Cadastro com senha com 5 caracteres', () => {
+  it('CT002 - Tentativa de cadastro com e-mail já cadastrado', () => {
+    cy.cadastro(name, "kaiqueteste@hotmail.com", password);
+    cy.url().should("eq", `${Cypress.config("baseUrl")}/register`);
+  });
+
+  it('CT003 - Tentativa de Cadastro com nome vazio', () => {
+    cy.cadastro(null, email, password);
+    cy.validaMsgErro(requiredNome);
+  });
+
+  it('CT004 - Tentativa de Cadastro com e-mail inválido', () => {
+    cy.cadastro(name, "email", password);
+    cy.validaMsgErro(requiredEmail);
+  });
+
+  it.only('CT005 - Tentativa de Cadastro com e-mail vazio', () => {
+    cy.cadastro(name, null, password);
+
+  });
+
+  it('CT006 - Tentativa de Cadastro com e-mail com números', () => {
+    cy.cadastro(name, email, password);
+    cy.get("#swal2-title").should("be.visible");
+    cy.contains("Cadastro realizado!").should("be.visible");
+    cy.contains(`Bem-vindo ${name}`).should("be.visible");
+    cy.url().should("eq", `${Cypress.config("baseUrl")}/my-account`);
+  }); 
+
+  it('CT007 - Tentativa de Cadastro com e-mail com caracter especial', () => {
+    cy.cadastro(name, email, password);
+    cy.get("#swal2-title").should("be.visible");
+    cy.contains("Cadastro realizado!").should("be.visible");
+    cy.contains(`Bem-vindo ${name}`).should("be.visible");
+    cy.url().should("eq", `${Cypress.config("baseUrl")}/my-account`);
+  });
+
+  it('CT008 - Tentativa de Cadastro com senha com 5 caracteres', () => {
     cy.cadastro(name, email, "tes12");
     cy.validaMsgErro(passwordMsg);
     cy.url().should("eq", `${Cypress.config("baseUrl")}/register`);
   });
 
-  it('CT017 - Tentativa de Cadastro com senha com 7 caracteres', () => {
+  it('CT009 - Tentativa de Cadastro com senha com 7 caracteres', () => {
     cy.cadastro(name, email, "teste12");
     cy.contains("Cadastro realizado!").should("be.visible");
     cy.contains(`Bem-vindo ${name}`).should("be.visible");
@@ -34,7 +71,13 @@ describe('Cadastro', () => {
     cy.url().should("eq", `${Cypress.config("baseUrl")}/my-account`);
   });
 
-  it('CT018 - Tentativa de Cadastro com senha vazia', () => {
+  it('CT010 - Tentativa de Cadastro com senha vazia', () => {
+    cy.cadastro(name, email, null);
+    cy.validaMsgErro(passwordMsg);
+    cy.url().should("eq", `${Cypress.config("baseUrl")}/register`);
+  });
+
+  it('CT011 - Tentativa de Cadastro com campos vazios', () => {
     cy.cadastro(name, email, null);
     cy.validaMsgErro(passwordMsg);
     cy.url().should("eq", `${Cypress.config("baseUrl")}/register`);
